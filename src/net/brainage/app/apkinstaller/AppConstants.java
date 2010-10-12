6206e17c5cedaf930ad71735412ad4e5f3b5cbfc
brainagenet <ms29.seo@gmail.com>
2010-10-12 23:51:24 +0900

    APK File 확장자 상수 추가.

--- src/net/brainage/app/apkinstaller/AppConstants.java
+++ src/net/brainage/app/apkinstaller/AppConstants.java
@@ -24,16 +24,20 @@ public final class AppConstants
     public static final boolean DEBUG = true;
 
     /**
-     * 
+     * 기본 디렉토리
      */
     public static final String SCAN_ROOT_DIRECTORY = "/mnt/sdcard/apps";
-    
-    
+
+    /**
+     * APK File 확장자
+     */
+    public static final String ANDROID_PACKAGE_FILE_EXT = ".apk";
+
     /**
      * 
      */
     public static final String ACTION_NEW_APPLICATION_FOUND = "net.brainage.apkinstaller.action.NEW_APPLICATION_FOUND";
-    
+
     public static final String ACTION_REFRESHED_APPLIST = "net.brainage.apkinstaller.action.REFRESHED_APPLIST";
 
 }
