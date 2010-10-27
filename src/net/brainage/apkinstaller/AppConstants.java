76d66ec9d8126eac16d3a7a154901bda5db57106
ntmyoungseok.seo <ms29.seo@gmail.com>
2010-10-27 12:15:59 +0900

    설정하기 화면 추가.

--- src/net/brainage/apkinstaller/AppConstants.java
+++ src/net/brainage/apkinstaller/AppConstants.java
@@ -21,7 +21,7 @@ public final class AppConstants
     /**
      * 디버그 여부 상수
      */
-    public static final boolean DEBUG = false;
+    public static final boolean DEBUG = true;
 
     /**
      * APK File 확장자
--- src/net/brainage/apkinstaller/ui/SettingsActivity.java
+++ src/net/brainage/apkinstaller/ui/SettingsActivity.java
@@ -4,9 +4,16 @@
  */
 package net.brainage.apkinstaller.ui;
 
+import java.io.File;
+
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.text.TextUtils;
 import android.util.Log;
 
 import net.brainage.apkinstaller.AppConstants;
@@ -56,6 +63,70 @@ public class SettingsActivity extends PreferenceActivity
         }
 
         addPreferencesFromResource(R.xml.preferences);
+
+        initializePreferenceScreen();
+    }
+
+    /**
+     * 
+     */
+    private void initializePreferenceScreen() {
+        PreferenceScreen ps = getPreferenceScreen();
+
+        // base directory preference
+        final CharSequence baseDirPreferenceKey = getResources().getText(
+                R.string.base_directory_key);
+        final EditTextPreference baseDirectoryPreference;
+        baseDirectoryPreference = (EditTextPreference) ps.findPreference(baseDirPreferenceKey);
+        baseDirectoryPreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                String oldBaseDirectory = baseDirectoryPreference.getText();
+                String newBaseDirectory = newValue.toString();
+
+                if ( DEBUG ) {
+                    Log.d(TAG, "onPreferenceChange(" + baseDirPreferenceKey
+                            + ") -----------------------------------");
+                    Log.d(TAG, "    - old value : " + oldBaseDirectory);
+                    Log.d(TAG, "    - new value : " + newBaseDirectory);
+                }
+
+                if ( oldBaseDirectory.equals(newBaseDirectory) ) {
+                    if ( DEBUG ) {
+                        Log.d(TAG, "    - same old value and new value");
+                    }
+                    return true;
+                }
+
+                if ( !newBaseDirectory.startsWith("/mnt/sdcard/") ) {
+                    String tmp = "/mnt/sdcard";
+                    if ( !newBaseDirectory.startsWith("/") ) {
+                        tmp += "/";
+                    }
+                    tmp += newBaseDirectory;
+                    newBaseDirectory = tmp;
+                }
+
+                File baseDir = new File(newBaseDirectory);
+                if ( !baseDir.exists() ) {
+                    if ( DEBUG ) {
+                        Log.d(TAG, "    - new dir was not exist and then create mkdirs.");
+                    }
+                    baseDir.mkdirs();
+                }
+
+                /* save new value to preference */
+                baseDirectoryPreference.setText(newBaseDirectory);
+
+                return false;
+            }
+        });
+
+        String value = baseDirectoryPreference.getText();
+        if ( TextUtils.isEmpty(value) ) {
+            String defaultValue = getResources().getText(R.string.default_base_directory)
+                    .toString();
+            baseDirectoryPreference.setText(defaultValue);
+        }
     }
 
 }
